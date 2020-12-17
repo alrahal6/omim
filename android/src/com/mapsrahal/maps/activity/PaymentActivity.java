146046222f08a249602b5cff8ab90d87fab9683c
sd <dhayal.cluzter@gmail.com>
2020-12-17 19:12:30 +0530

    payment page

--- android/src/com/mapsrahal/maps/activity/PaymentActivity.java
+++ android/src/com/mapsrahal/maps/activity/PaymentActivity.java
@@ -1,16 +1,30 @@
 package com.mapsrahal.maps.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.mapsrahal.maps.R;
+import com.mapsrahal.util.UiUtils;
 
 public class PaymentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        UiUtils.setupColorStatusBar(this,R.color.bg_statusbar);
         setContentView(R.layout.activity_payment);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.payment);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
